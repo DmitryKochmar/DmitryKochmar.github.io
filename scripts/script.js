24731fa91797214eae66c06c2fe7cbc14d4696f4
Dmitry Kochmar <dmitrykochmar@gmail.com>
2025-11-15 16:07:18 +0300

    Copy project from old folder.

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+// window.addEventListener('scroll', function() {
+//     const mainHeader = document.querySelector('.header');
+//     const triggerPoint100vh = window.innerHeight;
+//     if (window.scrollY > triggerPoint100vh) {
+//         mainHeader.classList.add('scrolled');
+//     } else {
+//         mainHeader.classList.remove('scrolled');
+//     }
+// });
